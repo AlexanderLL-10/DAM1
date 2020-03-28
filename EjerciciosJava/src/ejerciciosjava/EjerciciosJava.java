63e6f481f9d6f45b21f50f3c31b24d13ca2a5702
Admin-Alex <Admin-Alex@DESKTOP-GKGV4QL.home>
2020-03-28 19:10:44 +0100

    Prueba Git

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author Admin-Alex
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+     String nombre="Antonio Perez Romero";
+    int edad=20;
+    double altura=1.75;
+    System.out.println(nombre + "/" + edad + "/" + altura);
+            
+    }
+    
+}
